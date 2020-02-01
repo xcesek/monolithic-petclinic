b9be8035352a1c6bf77dd2304c123a61825324da
Peter Kofler <peter.kofler@code-cop.org>
2020-02-01 23:40:16 +0100

    Set up monolith to be JMS listener and publisher so copies work out of the box.

--- monolith/src/main/java/org/springframework/samples/petclinic/Application.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/Application.java
@@ -2,7 +2,9 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
+@EnableJms
 @SpringBootApplication(proxyBeanMethods = false)
 public class Application {
 

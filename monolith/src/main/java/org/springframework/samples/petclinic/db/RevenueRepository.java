dfc45520a88b89ab56d0f4951f5303570c34df0d
Peter Kofler <peter.kofler@code-cop.org>
2020-02-01 23:17:34 +0100

    Add Save to Revenue Repository which is needed after splitting it.

--- monolith/src/main/java/org/springframework/samples/petclinic/db/RevenueRepository.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/db/RevenueRepository.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.db;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.samples.petclinic.model.Visit;
@@ -16,5 +17,7 @@ public interface RevenueRepository extends Repository<Visit, Integer> {
         "from Visit v " +
         "group by YEAR(v.date)")
     List<YearlyRevenue> listYearlyRevenue();
+
+    void save(Visit visit) throws DataAccessException;
 
 }
